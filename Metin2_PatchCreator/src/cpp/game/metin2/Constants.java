8009fc31d8767804eaceb526f73e43bc86a9359f
iMerv3 <67241171+iMerv3@users.noreply.github.com>
2021-12-22 08:09:53 +0100

    Update Constants.java

--- Metin2_PatchCreator/src/cpp/game/metin2/Constants.java
+++ Metin2_PatchCreator/src/cpp/game/metin2/Constants.java
@@ -6,7 +6,7 @@ public class Constants
     public static final int patchLauncherVersion = 101; // version to package the launcher as
     public static final int numUpdateServers = 1; // number of update servers
 
-    public static final String baseDirectory = "C:\\Users\\iMerv3\\Metin2\\7. Autopatcher"; // working directory
+    public static final String baseDirectory = "path"; // working directory
     public static final String gameDirectory = baseDirectory + "\\client"; // the game files directory name in the working directory that the patch will be created of
     public static final String uploadDirectory = baseDirectory + "\\upload"; // the directory where the created patch files that need uploading go
     public static final String[] excludedFiles = {}; // files to exclude from the game files directory
